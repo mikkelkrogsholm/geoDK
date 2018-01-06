4c067506eb618616c3ebfa26454307ab03543e75
Mikkel Freltoft Krogsholm <mikkel@56n.dk>
2018-01-06 23:10:15 +0000

    Major rebuild

--- R/data.R
+++ R/data.R
@@ -1,20 +1,48 @@
-#' 135.376 danish place names
+#' Danish constituencies
 #'
-#' A data set containing danish place names for many different types of places.
-#' The dataset also contains coordinates for each place.
+#' @format SpatialPolygonsDataFrame
 #'
-#' @format A data frame with 135.376 rows and 9 variables:
-#' \describe{
-#'   \item{id}{id of place}
-#'   \item{name}{name of the place}
-#'   \item{type}{type of place - ie city or hill}
-#'   \item{lng}{longitude of the place}
-#'   \item{lat}{latitude of the place}
-#'   \item{municipalcode}{administrative code of the municipal}
-#'   \item{municipality}{name of the municipality}
-#'   \item{regionalcode}{administrative code of the region}
-#'   \item{region}{name of the region}
-#' }
-#' @source \url{http://download.kortforsyningen.dk/}
-"placenames"
+#' @source \url{https://kortforsyningen.dk/}
+"geo_con"
 
+#' Danish jurisdictions
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_jur"
+
+#' Danish municipalities
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_mun"
+
+#' Danish parishes
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_par"
+
+#' Danish police districts
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_pol"
+
+#' Danish regions
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_reg"
+
+#' Danish zip codes
+#'
+#' @format SpatialPolygonsDataFrame
+#'
+#' @source \url{https://kortforsyningen.dk/}
+"geo_zip"
